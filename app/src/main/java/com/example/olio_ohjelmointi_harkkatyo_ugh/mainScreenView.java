45de8a8d21197c9b4242066553d6dd0b15d9f6a5
xPertti <martin.peralahti@gmail.com>
2021-04-17 19:02:28 +0300

    päänäytön luokka ja jotai muokkauksia

---
+++ app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/mainScreenView.java
@@ -0,0 +1,14 @@
+package com.example.olio_ohjelmointi_harkkatyo_ugh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainScreenView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen_view);
+    }
+}
