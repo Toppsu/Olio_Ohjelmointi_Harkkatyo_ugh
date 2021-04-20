f7814ea62c2f4eca25056e5f80d7298852ffcb8a
Klapi <klapin.roskaposti@gmail.com>
2021-04-20 11:29:33 +0300

    Kirjautumisen onnetontä räpeltämistä

---
+++ app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/Login.java
@@ -0,0 +1,24 @@
+package com.example.olio_ohjelmointi_harkkatyo_ugh;
+
+import android.widget.Toast;
+
+
+public class Login {
+
+    public static void login(String username, String password){
+        boolean usernameFound = false;
+        boolean passwordCorrect = false;
+
+        //FIND THE USERNAME
+
+        //THEN COMPARE (secure)PASSWORD
+
+        if (usernameFound & passwordCorrect) {
+            //IF USERNAME FOUND AND PWD MATCHES, LET USER IN
+
+        } else {
+            //Error message
+        }
+
+    }
+}
