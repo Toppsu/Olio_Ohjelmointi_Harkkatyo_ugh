5bee38b104d03a76196bafdd4682ef04f7d6626d
Klapi <klapin.roskaposti@gmail.com>
2021-04-27 17:37:50 +0300

    Poisteltu testaamisen aikaisia println komentoja

--- app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/DataHolder.java
+++ app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/DataHolder.java
@@ -3,6 +3,8 @@ package com.example.olio_ohjelmointi_harkkatyo_ugh;
 import java.util.ArrayList;
 import java.util.List;
 
+//This holds the userlist and other
+
 public class DataHolder {
     List<User> userlist = new ArrayList<>();
     User currentUser;
