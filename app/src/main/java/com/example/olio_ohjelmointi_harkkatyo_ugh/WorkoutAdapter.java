f218b2636e2872ca0b888dbc970809d48a3e9b3b
xPertti <martin.peralahti@gmail.com>
2021-04-28 12:14:01 +0300

    Wörkoutin kikkoja

--- app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/WorkoutAdapter.java
+++ app/src/main/java/com/example/olio_ohjelmointi_harkkatyo_ugh/WorkoutAdapter.java
@@ -3,6 +3,7 @@ package com.example.olio_ohjelmointi_harkkatyo_ugh;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,15 @@ import java.util.ArrayList;
 public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.WorkoutViewHolder> {
 
     private ArrayList<Exercise> mExerciseArrayList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener{
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
 
     public static class WorkoutViewHolder extends RecyclerView.ViewHolder{
 
@@ -21,8 +31,9 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.WorkoutV
         public TextView repsTextView;
         public TextView weightsTextView;
         public TextView positionTextView;
+        public ImageView deleteImage;
 
-        public WorkoutViewHolder(@NonNull View itemView) {
+        public WorkoutViewHolder(@NonNull View itemView, OnItemClickListener listener) {
             super(itemView);
 
             exerciseTextView = itemView.findViewById(R.id.exerciseName);
@@ -30,6 +41,21 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.WorkoutV
             repsTextView = itemView.findViewById(R.id.reps);
             weightsTextView = itemView.findViewById(R.id.weights);
             positionTextView = itemView.findViewById(R.id.position);
+            deleteImage = itemView.findViewById(R.id.image_delete);
+
+
+
+            deleteImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position!=RecyclerView.NO_POSITION){
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -41,7 +67,7 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.WorkoutV
     @Override
     public WorkoutViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.exercise_item,parent,false);
-        WorkoutViewHolder wvh = new WorkoutViewHolder(v);
+        WorkoutViewHolder wvh = new WorkoutViewHolder(v,mListener);
         return wvh;
     }
 
